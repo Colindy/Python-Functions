5238beed47a113d11656f4685b494316c2356d3a
Colindy <cnellinger@yahoo.com>
2023-01-21 19:09:26 -0500

    Try It Yourself 8-1 and 8-2

---
+++ display_message.py
@@ -0,0 +1,4 @@
+def display_message():
+    print("Hi there, I'm a function.")
+
+display_message()
---
+++ favorite_book.py
@@ -0,0 +1,6 @@
+prompt = "What's your favorite book? "
+
+def favorite_book(title):
+    print(f"So your favorite book is {title.title()}.")
+
+favorite_book(input(prompt))
