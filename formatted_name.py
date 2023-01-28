c3a642de6334257011950b945bb30148bf218dcb
cnell <cnellinger@yahoo.com>
2023-01-27 20:00:55 -0500

    city_country.py is a work in progress

--- formatted_name.py
+++ formatted_name.py
@@ -12,3 +12,16 @@ badass = get_formated_name('ahsoka', 'tano')
 print(badass)
 musician = get_formated_name('james', 'keenan', 'herbert')
 print(musician)
+
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    f_name = input("First Name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last Name: ")
+    if l_name == 'q':
+        break
+
+    formatted_name = get_formated_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
