d3a9ffaa543cd90a9dc9f3a9babb24112e6b90ca
cnell <cnellinger@yahoo.com>
2023-03-13 17:42:08 -0400

    creating modules and importing them now!

---
+++ making_pizzas.py
@@ -0,0 +1,6 @@
+import pizza
+
+
+pizza.make_pizza('16', 'pepperoni')
+pizza.make_pizza('22', 'sausage', 'mushroom', 'extra cheese')
+
--- pizza.py
+++ pizza.py
@@ -1,8 +1,5 @@
-def make_pizza(*toppings):
+def make_pizza(size, *toppings):
     """Summarize the pizza we are making"""
-    print("\nMaking a pizza with the following items:")
+    print(f"\nMaking a {size}-inch pizza with the following items:")
     for topping in toppings:
         print(f"- {topping}")
-
-make_pizza('pepperoni')
-make_pizza('sausage', 'mushroom', 'extra cheese')
