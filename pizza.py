d4af0ab6089fac7dd129ab1a06b5af9f83f67b80
cnell <cnellinger@yahoo.com>
2023-03-13 14:34:16 -0400

    more changes

--- pizza.py
+++ pizza.py
@@ -1,5 +1,8 @@
 def make_pizza(*toppings):
-    print(toppings)
+    """Summarize the pizza we are making"""
+    print("\nMaking a pizza with the following items:")
+    for topping in toppings:
+        print(f"- {topping}")
 
 make_pizza('pepperoni')
 make_pizza('sausage', 'mushroom', 'extra cheese')
