90cd6658a5545c3ebdba118126652ef9f9474d26
Colindy <cnellinger@yahoo.com>
2023-03-14 21:16:07 -0400

    adding functions from other modules

--- making_pizzas.py
+++ making_pizzas.py
@@ -1,6 +1,10 @@
-import pizza
+# Here, I import a function from a module and give it a nickname
 
+from pizza import make_pizza as mp
 
-pizza.make_pizza('16', 'pepperoni')
-pizza.make_pizza('22', 'sausage', 'mushroom', 'extra cheese')
+# I can then take that nickname and use it to call the function
+# from the imported module.
+
+mp('16', 'pepperoni')
+mp('22', 'sausage', 'mushroom', 'extra cheese')
 
